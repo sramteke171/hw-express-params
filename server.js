c318f7f271641d2384dca86d73165deb05fd7c02
Sachin Ramteke <sachin06jd@gmail.com>
2020-07-16 16:46:55 -0500

    Commit2 - Tip Calculator

--- server.js
+++ server.js
@@ -23,10 +23,22 @@ app.get("/greeting/:name", (req, res) => {
     res.send(`Hello, What's up ${req.params.name}! It's so great to see you!`)
 });
 
-// app.get("/greeting", (req, res) => {
-//     console.log("req.param");
-//     res.send("Hello, stranger");
-// });
+
+// Tip Calculator
+// Your app should have a route of '/tip' and it should expect 2 params. One should be total and one should be tipPercentage.
+
+// When hitting the route, the page should display how much your tip will be based on the total amount of the bill and the tip percentage. (ex. hitting '/tip/100/20' should display 20 on the page).
+
+// Be sure you are res.send()ing a String. If you try to directly res.send() a Number, you will get errors about HTTP status codes.
+
+// 🔴 Commit 2
+
+let tip;
+app.get("/tip/:total/:tipPercentage", (req, res) => {
+    tip = req.params.total * (req.params.tipPercentage / 100);
+    console.log(req.params.tipPercentage);
+    res.send(`${tip}`)
+});
 
 app.listen(3000, () => {
     console.log("nodemon/server listening");
